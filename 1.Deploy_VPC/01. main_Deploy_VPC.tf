5508a4bf9c0865d437a1cd0d6e8b42bf6afdb89c
Apollinaire <apollinaire.moreno@yahoo.com>
2019-10-29 07:57:12 +0400

    Thursday update

--- 1.Deploy_VPC/01. main_Deploy_VPC.tf
+++ 1.Deploy_VPC/01. main_Deploy_VPC.tf
@@ -45,7 +45,7 @@ data "aws_vpc" "tf_existing_VPC" {
 }
 
 
-data "aws_subnet_ids" "tf_PublicSubnet2" {
+data "aws_subnet_ids" "find_PublicSubnet2" {
     vpc_id = "${data.aws_vpc.tf_existing_VPC.id}"
   tags = {
  #   Name = "FTGT-HA-2AZs-PublicSubnet2"
@@ -54,53 +54,60 @@ data "aws_subnet_ids" "tf_PublicSubnet2" {
 }
 
 data "aws_route_table" "find_PublicSubnet2RouteTableID" {
-    subnet_id = "${sort(data.aws_subnet_ids.tf_PublicSubnet2.ids)[0]}"
+    subnet_id = "${sort(data.aws_subnet_ids.find_PublicSubnet2.ids)[0]}"
 }
 
-#Export the Route Table ID
+#Export the id of Public Subnet 2 and the associated Route Table ID
 output "tf_PublicSubnet2RouteTableID" {
     value = "${data.aws_route_table.find_PublicSubnet2RouteTableID.route_table_id}"
 }
 
-
-output "existing_VPC_id" {
-  value = "${data.aws_vpc.tf_existing_VPC.id}"
-}
-output "debug_Sub2name" {
-value ="${var.tf_StackLabel}-PublicSubnet2"
-
-}
 output "tf_PublicSubnet2ID" {
-  value = "${data.aws_subnet_ids.tf_PublicSubnet2}"
+    value = "${sort(data.aws_subnet_ids.find_PublicSubnet2.ids) [0]}"
+
 }
 
 
+################################################
+### Next lines no longer needed - used to debug
+#output "existing_VPC_id" {
+#  value = "${data.aws_vpc.tf_existing_VPC.id}"
+#}
+#output "debug_Sub2name" {
+#value ="${var.tf_StackLabel}-PublicSubnet2"
 
-resource "aws_vpc" "Main_VPC" {
-    cidr_block           = "10.1.0.0/16"
-    enable_dns_hostnames = true
-    enable_dns_support   = true
-    instance_tenancy     = "default"
-
-    tags = {
-        "Name" : "Other_VPC"
-    }
-}
-
-
-data "aws_vpc" "foo" {
-depends_on = [aws_vpc.Main_VPC]
-cidr_block           = "10.1.0.0/16"
-  #tags = {
- #   service = "production"
- # }
-}
-
-output "foo" {
-  value = "${data.aws_vpc.foo.id}"
-}
+#}
+#output "tf_PublicSubnet2ID" {
+#  value = "${data.aws_subnet_ids.tf_PublicSubnet2}"
+#}
 
 
-output "debug_PublicSubnet2RouteTableID" {
-    value = "${aws_vpc.Main_VPC.main_route_table_id}"
-}
+
+#resource "aws_vpc" "Main_VPC" {
+#    cidr_block           = "10.1.0.0/16"
+#    enable_dns_hostnames = true
+#    enable_dns_support   = true
+#    instance_tenancy     = "default"
+#
+#    tags = {
+#        "Name" : "Other_VPC"
+#    }
+#}
+
+
+#data "aws_vpc" "foo" {
+#depends_on = [aws_vpc.Main_VPC]
+#cidr_block           = "10.1.0.0/16"
+#  #tags = {
+# #   service = "production"
+# # }
+#}
+
+##output "foo" {
+#  value = "${data.aws_vpc.foo.id}"
+#}
+
+
+#output "debug_PublicSubnet2RouteTableID" {
+#    value = "${aws_vpc.Main_VPC.main_route_table_id}"
+#}
